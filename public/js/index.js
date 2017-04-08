33c7dd888969e559ca6798a2e1f2f613f2361b44
Harman Grewal <Harman Grewal>
2017-04-09 03:18:09 +0530

    Auto Scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 // iteating the request
 var socket = io();
+
+function scrollToBottom () {
+  // Selectors
+var messages = jQuery('#messages');
+var newMessage = messages.children('li:last-child');
+
+  //Heights
+
+var clientHeight = messages.prop('clientHeight');
+var scrollTop = messages.prop('scrollTop');
+var scrollHeight = messages.prop('scrollHeight');
+var newMessageHeight = newMessage.innerHeight();
+var lastMessageHeight = newMessage.prev().innerHeight();
+if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function()  {  //Inbuild eveent
   console.log('Connected to server');
 });
@@ -18,6 +36,7 @@ socket.on('newMessage', function(message){
       });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // console.log(message);
   // var li  = jQuery('<li></li>');
@@ -35,6 +54,7 @@ socket.on('newLocationMessage', function(message){
       });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target = "_blank">My Current Location</a>');
   //
